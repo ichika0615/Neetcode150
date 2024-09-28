aeb45bfc873d10a69aec2acfac6b15d915043e79
Shu Maeda <shu2022shu@docomo.ne.jp>
2024-09-28 19:50:29 +0900

    Update nc5_top_k_elements.py

--- nc5_top_k_elements.py
+++ nc5_top_k_elements.py
@@ -1,16 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for _ in range(len(nums) + 1)]
-
+        count = {} # key:num  val:occurence
+        
+        freq = [[] for _ in range(len(nums) + 1)] # idx:occurence val: list of nums that occure certain times
+        
+        #count
         for n in nums:
             count[n] = 1 + count.get(n, 0)
+        #freq
         for n, c in count.items():
             freq[c].append(n)
-
+            
         res = []
         for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
                     return res
+                    
