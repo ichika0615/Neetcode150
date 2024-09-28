5ce8aad5e0b1617c73bf9a2ea9c5cf409a8a2109
Shu Maeda <shu2022shu@docomo.ne.jp>
2024-09-28 14:38:13 +0900

    Update nc1_contains_duplicate.py
    brute force solutions is added

--- nc1_contains_duplicate.py
+++ nc1_contains_duplicate.py
@@ -8,3 +8,15 @@ class Solution:
              hashset.add(n)
         #if no n is in hashset, nothing is duplicate.
          return False
+#tc: O(n)  sc:O(n)
+
+#if brute force is used
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+       for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+         return False
+#tc: O(n*n) sc: O(1)
+
