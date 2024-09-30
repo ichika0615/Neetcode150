72b7b16bcea15fb14563afcc753aba9fc93a4a39
Shu Maeda <shu2022shu@docomo.ne.jp>
2024-09-30 20:24:07 +0900

    Update nc4_anagram_groups.py
    some explanations are added

--- nc4_anagram_groups.py
+++ nc4_anagram_groups.py
@@ -1,9 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)  # mapping charCount to list of Anagrams
+        res = defaultdict(list)  # key: tuple of what alphabets occure and how many times 
+                                 # val: list of strings that satisfy their keys.
 
         for s in strs:
-            count = [0] * 26  # a ... z
+            count = [0] * 26  # a ... z # count := [0, 0, ..., 0]
 
             for c in s:
                 count[ord(c) - ord('a')] += 1
